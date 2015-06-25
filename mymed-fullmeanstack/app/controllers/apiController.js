22b773833a4b195c44318477ad93bf834e5e7e16
virendra chouhan <chouhan.virendra@gmai.com>
2015-06-25 18:08:39 +0530

    Full MEAN Stack Prototype Commit
    https://xfinion.atlassian.net/browse/AGILEBPA-16

---
+++ mymed-fullmeanstack/app/controllers/apiController.js
@@ -0,0 +1,58 @@
+var locomotive = require('locomotive'), 
+Controller = locomotive.Controller;
+var request = require('request');
+var mymed = require('../models/mymed');
+var ObjectId = require('mongoose').Types.ObjectId;
+var apiController = new Controller();
+
+apiController.getSearchResults = function(res,req) {
+  var th = this;
+  var query = th.req.param('q');
+  var url = "https://api.fda.gov/drug/label.json?search=effective_time:[20090601+TO+20140731]+AND+openfda.substance_name:"+query+"&limit=10";
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var d = JSON.parse(body);
+      th.res.json({success:true, data : d.results});
+    }
+  });
+}
+
+apiController.saveData = function(res,req) {
+  var th = this;
+  var data = th.req.body.mydata;
+  var newmymed = new mymed(th.req.body); 
+  newmymed.save(function (err) {
+      if(err) {
+          return th.res.json({success: false, id: ''});
+      } else {
+        th.res.json({success: true, id: newmymed._id});
+      }
+    });
+}
+
+apiController.updateData = function(res,req) {
+  var th = this;
+  var id = th.req.body.id;
+  var data = th.req.body.data;
+  mymed.update({_id: ObjectId(id)}, {$set: {data: data}}, function(e,r){
+    if(r){
+      return th.res.json({success: true});      
+    } else {
+      return th.res.json({success: false});     
+    }
+  });
+}
+
+apiController.getData = function(res,req) {
+  var th = this;
+  var id = th.req.param('id');
+  mymed.findOne({_id: ObjectId(id)}, function(e,r){
+    if(r){
+      th.res.json({success: true, result: r});
+    }
+    else{
+      th.res.json({success: false, result: []});
+    }
+  });
+}
+module.exports = apiController;
