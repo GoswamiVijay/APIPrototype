e62914cf315e89593c860d6b730820c8487c0d25
Vijay Goswami <goswamivijay@yahoo.com>
2015-06-27 15:39:01 -0400

    Added nginx support

--- config/initializers/04_database.js
+++ config/initializers/04_database.js
@@ -1,6 +1,6 @@
 module.exports = function() {
   var mongoose = require('mongoose'),
-  dbAddress = ['mongodb://192.168.59.103:27017/mymed'];
+  dbAddress = ['mongodb://mymedlookupdb:27017/mymed'];
   mongoose.connect(dbAddress);
 
 }
