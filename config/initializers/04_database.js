6e7d4f267aeb19139095c42767d30e9c193ab248
Vijay Goswami <goswamivijay@yahoo.com>
2015-06-29 23:05:57 -0400

    Fix/update database servername

--- config/initializers/04_database.js
+++ config/initializers/04_database.js
@@ -5,7 +5,7 @@ module.exports = function() {
   //uncomment if you are using mongodb with docker
   //dbAddress = ['mongodb://mymedlookupdb:27017/mymed'];
   //uncomment for production deployment
-  dbAddress = ['mongodb://50.0.0.200:27017/mymed'];
+  dbAddress = ['mongodb://50.0.0.172:27017/mymed'];
   mongoose.connect(dbAddress);
 
 }
