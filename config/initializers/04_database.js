294d4fcfaad5fd153ba5d75336cead3ad5ce1650
Vijay Goswami <goswamivijay@yahoo.com>
2015-06-29 23:12:33 -0400

    Updated database url for test environment

--- config/initializers/04_database.js
+++ config/initializers/04_database.js
@@ -5,7 +5,7 @@ module.exports = function() {
   //uncomment if you are using mongodb with docker
   //dbAddress = ['mongodb://mymedlookupdb:27017/mymed'];
   //uncomment for production deployment
-  dbAddress = ['mongodb://54.165.225.158:27017/mymed'];
+  dbAddress = ['mongodb://60.0.0.81:27017/mymed'];
   mongoose.connect(dbAddress);
 
 }
