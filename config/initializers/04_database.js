f385f3133b52c0e4aac626b9bb7bda464ff49dd2
Vijay Goswami <goswamivijay@yahoo.com>
2015-06-30 13:41:56 -0400

    Added docker file specific to local development on docker

--- config/initializers/04_database.js
+++ config/initializers/04_database.js
@@ -1,10 +1,13 @@
 module.exports = function() {
-  var mongoose = require('mongoose'),
+  var databaseserver  = process.env.databaseserver || 'localhost';
+  var mongoose = require('mongoose');
   //uncomment if you are using mongodb on your local box
   //dbAddress = ['mongodb://localhost:27017/mymed'];
   //uncomment if you are using mongodb with docker
   //dbAddress = ['mongodb://mymedlookupdb:27017/mymed'];
   //uncomment for production deployment
-  dbAddress = ['mongodb://70.0.0.234:27017/mymed'];
+  
+  //console.log('databaseserver is - ' + databaseserver);
+  dbAddress = ['mongodb://'+ databaseserver +':27017/mymed'];
   mongoose.connect(dbAddress);
 }
